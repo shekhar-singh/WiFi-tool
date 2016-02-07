9e2ae2a2a5434c24e1268244a5225e2a39cfc264
shekhar-singh <sschoudhary60@gmail.com>
2016-02-07 19:57:45 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Wifi-Tool',
+    version='0.1.0',
+    description='Wifi analysis and connecting tool',
+    author='Shekhar Singh Choudhary',
+    author_email='sschoudhary60@gmail.com',
+    url='https://github.com/shekhar-singh/WiFi-tool',
+    packages=['WiFi-tool'],
+    install_requires=[
+        'termcolor-1.1.0'
+    )
